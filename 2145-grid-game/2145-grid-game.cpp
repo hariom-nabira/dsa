7a45de52799734d43510767f8a286ff3ea27e46e
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-01-21 21:22:53 +0530

    Time: 0 ms (100%), Space: 66.7 MB (76.92%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n=grid[0].size();
-        vector<long long> preSumDown(n+1), postSumUp(n+1);
+        long long ans = LLONG_MAX, topSum = 0, bottomSum = 0;
         for(int i=0; i<n; i++){
-            preSumDown[i+1] = preSumDown[i] + grid[1][i];
-            postSumUp[n-i-1] = postSumUp[n-i] + grid[0][n-i-1];
+            topSum += grid[0][i];
         }
-        long long ans = LLONG_MAX;
         for(int i=0; i<n; i++){
-            ans = min(ans, max(preSumDown[i],postSumUp[i+1]));
+            topSum -= grid[0][i];
+            ans = min(ans, max(bottomSum, topSum));
+            bottomSum += grid[1][i];
         }
         return ans;
     }
