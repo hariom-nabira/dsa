fdb53fa81c1337ac30ef7bdc4afbac0f81941ec4
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-01-23 10:47:20 +0530

    Time: 71 ms (84.56%), Space: 113 MB (59.67%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -3,32 +3,34 @@ public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         //multi-source bfs
         int n = isWater.size(), m = isWater[0].size();
-        vector<vector<int>> ans(n, vector<int>(m));
+        int dx[4] = {-1,1,0,0};
+        int dy[4] = {0,0,-1,1};
+        vector<vector<int>> ans(n, vector<int>(m,-1));
         queue<pair<int,int>> q;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(isWater[i][j]) q.push({i,j});
+                if(isWater[i][j]){
+                    ans[i][j] = 0;
+                    q.push({i,j});
+                }
             }
         }
         int len=1;
         while(!q.empty()){
             int sz = q.size();
             for(int k=0; k<sz; k++){
-                auto [i,j] = q.front();
+                auto &[i,j] = q.front();
+                for(int d=0; d<4; d++){
+                    int x = i+dx[d], y = j+dy[d];
+                    if(x>=0 && x<n && y>=0 && y<m && ans[x][y]==-1){
+                        ans[x][y] = len;
+                        q.push({x,y});
+                    }
+                }
                 q.pop();
-                checkAndSet(i+1, j, n, m, len, isWater, ans, q);
-                checkAndSet(i-1, j, n, m, len, isWater, ans, q);
-                checkAndSet(i, j+1, n, m, len, isWater, ans, q);
-                checkAndSet(i, j-1, n, m, len, isWater, ans, q);
             }
             len++;
         }
         return ans;
-    }
-    void checkAndSet(int i, int j, int n, int m, int len, vector<vector<int>> &isWater, vector<vector<int>> &ans, queue<pair<int,int>> & q){
-        if(i<0 || i==n || j<0 || j==m || isWater[i][j]) return;
-        isWater[i][j] = 1;
-        ans[i][j] = len;
-        q.push({i,j});
     }
 };
