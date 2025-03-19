883519c8f910b0b793fc4d4bcf01ea179b8df78c
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-03-19 16:51:45 +0530

    Time: 0 ms (100%), Space: 60.9 MB (87.22%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -2,38 +2,17 @@ class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
         int l=0, n=nums.size(), ans=1;
-        map<int, int> bitPosFreq;
+        int mask=0;
         for(int r=0; r<n; r++){
-            //adding nums[r] to curr
-            int temp = nums[r], bit=0;
-            while(temp>0){
-                if(temp&1){
-                    bitPosFreq[bit]++;
-                }
-                temp>>=1;
-                bit++;
-            }
             //shrinking the window by removing nums[l] from curr
-            while(!isValid(bitPosFreq)){
-                int temp = nums[l++], bit=0;
-                while(temp>0){
-                    if(temp&1){
-                        bitPosFreq[bit]--;
-                    }
-                    temp>>=1;
-                    bit++;
-                }
+            while(mask & nums[r]){
+                mask ^= nums[l++];
             }
+            //adding nums[r] to curr
+            mask |= nums[r];
             //updating ans
             ans = max(ans, r-l+1);
         }
         return ans;
-    }
-private:
-    bool isValid(map<int,int> &mp){
-        for(auto &p:mp){
-            if(p.second>1) return false;
-        }
-        return true;
     }
 };
