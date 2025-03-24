77d3a056f8f64e10235e90eb4cae38f230639062
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-03-24 23:06:43 +0530

    Time: 63 ms (79.59%), Space: 126.6 MB (59.18%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,18 +1,16 @@
-//same logic, cleaner implementation
+//same logic, much cleaner implementation (editorial)
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        int ans = 0;
-        sort(meetings.begin(),meetings.end());
-        ans += meetings[0][0]-1;
-        int prevEnd = meetings[0][1];
-        for(int i=1; i<meetings.size(); i++){
-            if(meetings[i][0]>prevEnd){
-                ans += meetings[i][0]-prevEnd-1; 
+        int freeDays = 0, latestEnd = 0;
+        sort(meetings.begin(), meetings.end());
+        for (auto& meeting : meetings) {
+            if (meeting[0] > latestEnd + 1) {
+                freeDays += meeting[0] - latestEnd - 1;
             }
-            prevEnd = max(prevEnd,meetings[i][1]);
+            latestEnd = max(latestEnd, meeting[1]);
         }
-        ans += days - prevEnd;
-        return ans; 
+        freeDays += days - latestEnd;
+        return freeDays;
     }
 };
