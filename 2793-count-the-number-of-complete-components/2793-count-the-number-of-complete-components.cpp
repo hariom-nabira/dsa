6f057a1664e1b5bdd3686e808395cebec32e3321
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-03-31 08:52:27 +0530

    Time: 11 ms (92.79%), Space: 117 MB (97.27%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,76 +1,32 @@
-//cfs
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        // adjacency lists for each vertex
-        vector<vector<int>> graph(n);
-        // map to store frequency of each unique adjacency list
-        unordered_map<string, int> componentFreq;
-
-        // initialize adjacency lists with self-loops
-        for (int vertex = 0; vertex < n; vertex++) {
-            graph[vertex].push_back(vertex);
+        par.resize(n, -1);
+        size.resize(n, 1);
+        edgesInComponent.resize(n, 0);
+        for(auto &e: edges){
+            myUnion(e[0], e[1]);
         }
-
-        // build adjacency lists from edges
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+        int ans=0;
+        for(int i=0; i<n; i++){
+            if(size[i] && edgesInComponent[i]==((size[i]*(size[i]-1))/2)) ans++;
         }
-
-        // count frequency of each unique adjacency pattern
-        for (int vertex = 0; vertex < n; vertex++) {
-            vector<int> neighbors = graph[vertex];
-            sort(neighbors.begin(), neighbors.end());
-
-            // convert vector to string for hashing
-            string key;
-            for (int num : neighbors) {
-                key += to_string(num) + ",";
-            }
-            componentFreq[key]++;
-        }
-
-        // count complete components where size equals frequency
-        int completeCount = 0;
-        for (const auto& entry : componentFreq) {
-            // count commas to get original vector size
-            int size = count(entry.first.begin(), entry.first.end(), ',');
-            if (size == entry.second) {
-                completeCount++;
-            }
-        }
-
-        return completeCount;
+        return ans;
+    }
+private:
+    vector<int> par, size, edgesInComponent;
+    int find(int i){
+        if(par[i]==-1) return i;
+        return par[i] = find(par[i]);
+    }
+    void myUnion(int x, int y){
+        int r1 = find(x), r2 = find(y);
+        if(size[r1]<size[r2]) swap(r1,r2);
+        edgesInComponent[r1]++;
+        if(r1==r2) return;
+        edgesInComponent[r1] += edgesInComponent[r2];
+        size[r1] += size[r2];
+        size[r2]=0;
+        par[r2] = r1;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     vector<int> parent, depth;
-//     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-//         parent.resize(n);
-//         depth.resize(n,1);
-//         for(int i=0; i<n; i++) parent[i] = i;
-//         for(auto &e:edges){
-//             myUnion(e[0], e[1]);
-//         }
-//         unordered_set<int> s;
-//         for(auto &i: parent) s.insert(find(i));
-//         return (int)s.size();
-//     }
-// private:
-//     int find(int i){
-//         if(parent[i]==i) return i;
-//         return parent[i] = find(parent[i]);
-//     }
-//     void myUnion(int x, int y){
-//         int r1 = find(x);
-//         int r2 = find(y);
-//         if(depth[r1]<depth[r2]) swap(r1,r2);
-//         if(depth[r1]==depth[r2]) depth[r1]++;
-//         parent[r2] = r1;
-//     }
-// };
