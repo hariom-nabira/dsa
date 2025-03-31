715068eae9018179cb0ca375c3d1fb8dfe4e186d
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-03-31 12:12:04 +0530

    Time: 30 ms (91.14%), Space: 61.6 MB (97.05%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
         int n = weights.size();
-        long long mn = weights[0]+weights[n-1];
-        long long mx = mn;
-        vector<long long> options;
-        for(int i=0; i<n-1; i++) options.push_back(weights[i]+weights[i+1]);
-        sort(options.begin(), options.end());
+        // long long mn = weights[0]+weights[n-1];
+        // long long mx = mn; (since they get cancelled out anyways when mx-mn)
+        long long ans=0;
+        for(int i=0; i<n-1; i++) weights[i] += weights[i+1];
+        weights.pop_back();
+        sort(weights.begin(), weights.end());
         for(int i=0; i<k-1; i++){
-            mn += options[i];
-            mx += options[n-2-i];
+            ans -= weights[i];
+            ans += weights[n-2-i];
         }
-        return mx-mn;
+        return ans;
     }
 };
