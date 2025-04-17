faf3e252304dc15a81c330268452d5221c520256
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-04-17 23:56:42 +0530

    Time: 4 ms (38.12%), Space: 10.3 MB (56.81%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,33 +1,34 @@
 class Solution {
-public:
-    void helper(int r, vector<int> &placed, vector<string> &curr, vector<vector<string>> &ans){
-        int n=curr.size();
+private:
+    bool canPlace(int r, int c, vector<int> &rowToCol){
+        for(int i=0; i<r; i++){
+            if(rowToCol[i] == c) return false;
+            if(abs(r-i) == abs(rowToCol[i]-c)) return false;
+        }
+        return true;
+    }
+    void helper(int r, vector<int> &rowToCol, vector<string> &curr, vector<vector<string>> &ans){
+        int n = rowToCol.size();
         if(r==n){
             ans.push_back(curr);
             return;
         }
-        for(int j=0; j<n; j++){
-            bool canPlace = true;
-            for(int i=0; i<r; i++){
-                if(placed[i]==j || abs(placed[i]-j) == (r-i)){
-                    canPlace=false;
-                    break;
-                }
-            }
-            if(canPlace){
-                curr[r][j] = 'Q';
-                placed[r] = j;
-                helper(r+1, placed, curr, ans);
-                placed[r] = -1;
-                curr[r][j] = '.';
+        for(int c=0; c<n; c++){
+            if(canPlace(r,c,rowToCol)){
+                curr[r][c] = 'Q';
+                rowToCol[r] = c;
+                helper(r+1, rowToCol, curr, ans);
+                rowToCol[r] = 0;
+                curr[r][c] = '.';
             }
         }
     }
+public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> curr(n, string(n, '.'));
-        vector<int> placed(n, -1);
-        helper(0,placed,curr,ans);
+        vector<string> curr(n, string(n,'.'));
+        vector<int> rowToCol(n, -1);
+        helper(0,rowToCol,curr,ans);
         return ans;
     }
 };
