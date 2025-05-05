ac827d9327f090237dbb15e48c197bf8307dfe66
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-05-05 21:57:50 +0530

    Time: 2 ms (26.63%), Space: 8.8 MB (61.7%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -3,14 +3,16 @@ private:
     int MOD = 1e9+7;
 public:
     int numTilings(int n) {
+        if(n<=1) return 1;
+        else if(n==2) return 2;
+        else if(n==3) return 5;
         vector<int> dp(n+1, -1);
         dp[0] = 1;
         dp[1] = 1;
-        for(int i=2; i<=n; i++){
-            dp[i] = (dp[i-1] + dp[i-2]) % MOD;
-            for(int j=i-3; j>=0; j--){
-                dp[i] = (dp[i] + (2*dp[j])%MOD ) %MOD;
-            }
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i=4; i<=n; i++){
+            dp[i] = (dp[i-1]*2LL + dp[i-3]) % MOD;
         }
         return dp[n];
     }
