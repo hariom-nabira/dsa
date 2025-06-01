604c79b6dd8d136f2465ddeeb0d30aa0795c2090
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-06-01 13:45:13 +0530

    Time: 9 ms (9.58%), Space: 25.8 MB (5.04%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int>candies(n, 0);
+        vector<int> candies(n, 1);
         vector<pair<int,int>> a;
         for(int i=0; i<n; i++){
             a.push_back({ratings[i], i});
         }
         sort(a.begin(), a.end());
         for(auto [r,ind]: a){
-            int mx=0;
-            if(ind-1>=0) mx = max(mx, (ratings[ind]==ratings[ind-1])? 0:candies[ind-1]);
-            if(ind+1<n) mx = max(mx, (ratings[ind]==ratings[ind+1])? 0:candies[ind+1]);
-            candies[ind] = mx+1;
+            if(ind-1>=0 && ratings[ind]>ratings[ind-1]) candies[ind] = max(candies[ind], candies[ind-1] + 1);
+            if(ind+1<n && ratings[ind]>ratings[ind+1]) candies[ind] = max(candies[ind], candies[ind+1] + 1);
+            // candies[ind]++;
         }
         return accumulate(candies.begin(), candies.end(), 0);
     }
