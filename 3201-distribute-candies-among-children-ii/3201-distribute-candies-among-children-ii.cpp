cc6f0377c34f047f85214130b393b5055aff7368
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-06-01 22:16:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+private:
+    long long nC2(int n){
+        if(n<=0) return 0;
+        return (1LL*n*(n-1))/2;
+    }
+public:
+    long long distributeCandies(int n, int limit) {
+        return nC2(n +2) - 3* nC2(n-(limit+1) +2) + 3* nC2(n-2*(limit+1) +2) - nC2(n - 3*(limit+1));
+    }
+};
