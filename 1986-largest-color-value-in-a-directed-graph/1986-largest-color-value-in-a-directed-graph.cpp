cf9964913d4bab66af661520b9020878d705368a
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-06-03 08:53:06 +0530

    Time: 356 ms (63.95%), Space: 189.4 MB (45.34%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
@@ -1,47 +1,31 @@
 class Solution {
 private:
-    int helper(int node, int color, vector<vector<int>> &dp, vector<vector<int>> &mp, string &colors){
-        if(dp[node][color]!=-1) return dp[node][color];
-        int ans = 0;
-        for(auto &ch : mp[node]){
-            ans = max(ans, helper(ch,color,dp,mp,colors));
+    int helper(int node, vector<vector<int>> &dp, vector<vector<int>> &mp, string &colors, vector<int> &visited){
+        if(!visited[node]){
+            visited[node] = 1;
+            for(auto &ch:mp[node]){
+                if(helper(ch,dp,mp,colors,visited)==INT_MAX) return INT_MAX;
+                for(int c=0; c<26; c++){
+                    dp[node][c] = max(dp[node][c], dp[ch][c]);
+                }
+            }
+            dp[node][colors[node]-'a']++;
+            visited[node] = 2;
         }
-        if(colors[node] == ('a'+color)) ans++;
-        return dp[node][color] = ans;
+        return visited[node]==2? dp[node][colors[node]-'a']:INT_MAX;
     }
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.size();
-        vector<vector<int>> mp(n);
-        vector<int> inDeg(n);
+        vector<vector<int>> mp(n), dp(n, vector<int>(26));
+        vector<int> visited(n);
         for(auto &e: edges){
             mp[e[0]].push_back(e[1]);
-            inDeg[e[1]]++;
         }
-        int ans=0, done=0;
-        queue<int> q;
-        for(int i=0; i<n; i++){
-            if(!inDeg[i]){
-                q.push(i);
-            }
+        int ans=0;
+        for(int i=0; i<n && ans!=INT_MAX; i++){
+            ans = max(ans, helper(i,dp,mp,colors,visited));
         }
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            done++;
-            for(auto &c: mp[node]){
-                if(--inDeg[c]==0){
-                    q.push(c);
-                }
-            }
-        }
-        if(done!=n) return -1;
-        vector<vector<int>> dp(n, vector<int>(26, -1));
-        for(int i=0; i<n; i++){
-            for(int c=0; c<26; c++){
-                ans = max(ans, helper(i,c,dp,mp,colors));
-            }
-        }
-        return ans;
+        return (ans==INT_MAX)? -1:ans;
     }
 };
