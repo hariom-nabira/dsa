cf526513c4b9a5b898c8c47dfbe99837c7540806
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-06-04 11:42:12 +0530

    Time: 4 ms (58.53%), Space: 44.1 MB (78.34%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -3,23 +3,32 @@ public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
         int n = candies.size(), ans=0;
         vector<bool> found(n);
-        for(auto &b : initialBoxes) found[b] = true;
         queue<int> q;
-        for(int i=0; i<n; i++){
-            if(found[i] && status[i]) q.push(i);
+        for(auto &b : initialBoxes){
+            found[b] = true;
+            if(status[b]){
+                q.push(b);
+                ans += candies[b];
+                candies[b] = 0;
+            }
         }
         while(!q.empty()){
             int box = q.front(); q.pop();
-            if(!candies[box]) continue;
-            ans += candies[box];
-            candies[box] = 0;
             for(auto b: keys[box]){
                 status[b] = 1;
-                if(found[b] && candies[b]) q.push(b);
+                if(found[b] && candies[b]){
+                    q.push(b);
+                    ans += candies[b];
+                    candies[b] = 0;
+                }
             }
             for(auto b: containedBoxes[box]){
                 found[b] = true;
-                if(status[b] && candies[b]) q.push(b);
+                if(status[b] && candies[b]){
+                    q.push(b);
+                    ans += candies[b];
+                    candies[b] = 0;
+                }
             }
         }
         return ans;
