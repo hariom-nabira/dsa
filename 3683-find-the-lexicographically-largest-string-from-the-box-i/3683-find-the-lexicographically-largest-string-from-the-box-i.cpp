a272997d40fa2fd4dcb184fb8451dbb5a5ec4b7f
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-06-10 15:33:49 +0530

    Time: 8 ms (85.92%), Space: 39.1 MB (68.07%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,31 +1,17 @@
-//cfs
-
 class Solution {
 public:
-    string lastSubstring(string s) {
-        int i = 0, j = 1, n = s.size();
-        while (j < n) {
-            int k = 0;
-            while (j + k < n && s[i + k] == s[j + k]) {
-                k++;
-            }
-            if (j + k < n && s[i + k] < s[j + k]) {
-                int t = i;
-                i = j;
-                j = max(j + 1, t + k + 1);
-            } else {
-                j = j + k + 1;
-            }
-        }
-        return s.substr(i, n - i);
-    }
-
     string answerString(string word, int numFriends) {
-        if (numFriends == 1) {
-            return word;
+        if(numFriends==1) return word;
+        vector<int> freq(26);
+        for(auto &ch: word) freq[ch-'a']++;
+        int ch=25, n=word.size();
+        while(!freq[ch]) ch--;
+        priority_queue<string> pq;
+        for(int i=0; i<n; i++){
+            if(word[i]==('a'+ch)){
+                pq.push(word.substr(i, n-numFriends+1));
+            }
         }
-        string last = lastSubstring(word);
-        int n = word.size(), m = last.size();
-        return last.substr(0, min(m, n - numFriends + 1));
+        return pq.top();
     }
 };
