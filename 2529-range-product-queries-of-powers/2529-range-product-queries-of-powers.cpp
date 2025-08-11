3ba9a6be67d75e2b4e9201c99bb1c405d4cee484
hariom-nabira <149952162+hariom-nabira@users.noreply.github.com>
2025-08-11 21:12:55 +0530

    Time: 10 ms (86.17%), Space: 147.9 MB (45.21%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -5,16 +5,20 @@ public:
         int i=1, MOD=1e9+7;
         while(n>0){
             if(n&1) powers.push_back(i);
-            i<<=1;
-            n>>=1;
+            i<<=1; n>>=1;
+        }
+        int p=powers.size();
+        vector<vector<int>> ans(p, vector<int>(p));
+        for(int i=0; i<p; i++){
+            int curr=1;
+            for(int j=i; j<p; j++){
+                curr = (1LL*powers[j]*curr)%MOD;
+                ans[i][j] = curr;
+            }
         }
         vector<int> res;
         for(auto &q: queries){
-            int ans = 1;
-            for(int i=q[0]; i<=q[1]; i++){
-                ans = (1LL*ans*powers[i])%MOD;
-            }
-            res.push_back(ans);
+            res.push_back(ans[q[0]][q[1]]);
         }
         return res;
     }
