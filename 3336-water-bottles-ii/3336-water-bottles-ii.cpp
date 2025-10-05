2bffab04f4159004162763e71f4fc870fd1b2ac9
Hariom Nabira <nabira.hariom@gmail.com>
2025-10-05 15:33:03 +0530

    Time: 0 ms (100%), Space: 8.5 MB (47.68%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.cpp
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int maxBottlesDrunk(int nb, int ne) {
-        int ans = nb, empty = nb;
-        while(empty >= ne){
-            empty -= (ne-1);
-            ne++; 
-            ans++;
-        }
-        return ans;
+        int a = 1, b = 2*ne-3, c = -2*nb;
+        int d = b*b - 4*a*c;
+        int t = ceil((-b + sqrt(d))/(2*a));
+        return nb+t-1;
     }
 };
